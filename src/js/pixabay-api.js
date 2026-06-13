0ad69c3e07eb43a51c6ceb75c3ac0232a771c15b
Златослава Вишківська <zlatoslava6b@gmail.com>
2026-06-13 14:32:12 +0300

    make refactoring

--- src/js/pixabay-api.js
+++ src/js/pixabay-api.js
@@ -1,15 +1,16 @@
 import axios from 'axios';
 
-export function getImagesByQuery(query) {
-  return axios
-    .get('https://pixabay.com/api/', {
-      params: {
-        key: '56277976-7813695b076e89130ccc466e1',
-        q: query,
-        image_type: 'photo',
-        orientation: 'horizontal',
-        safesearch: true,
-      },
-    })
-    .then(response => response.data);
+export async function getImagesByQuery(query, page) {
+  const response = await axios.get('https://pixabay.com/api/', {
+    params: {
+      key: '56277976-7813695b076e89130ccc466e1',
+      q: query,
+      image_type: 'photo',
+      orientation: 'horizontal',
+      safesearch: true,
+      page: page,
+      per_page: 15,
+    },
+  });
+  return response.data;
 }
